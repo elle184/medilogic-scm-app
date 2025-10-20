c1348d3f8032eca49331cb3e0e3d38fe1ddfcdc3
Adrián Francisco Becerra Arias <a.becerraa@uniandes.edu.co>
2025-10-19 23:02:54 -0500

    Versión inicial de la aplicación.

--- App.js
+++ App.js
@@ -1,10 +1,90 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
+  const [clientList, setClientList] = useState([]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading1}>Mis asignaciones</Text>
+      <ScrollView>
+        <View style={styles.containerItem}>
+          <Text>Realcube</Text>
+          <Text>Axel-Springer-Platz 3</Text>
+          <Text>Lorie Aguilar</Text>
+        </View>
+        <View style={styles.containerItem}>
+          <Text>Skyba</Text>
+          <Text>Bredgade 10</Text>
+          <Text>Hernando Downham</Text>
+        </View>
+        <View style={styles.containerItem}>
+          <Text>Tagcat</Text>
+          <Text>Carnegieplein 4</Text>
+          <Text>Anitra Halfhead</Text>
+        </View>
+        <View style={styles.containerItem}>
+          <Text>Centidel</Text>
+          <Text>150 1ST STREET S E</Text>
+          <Text>Carri Verbruggen</Text>
+        </View>
+        <View style={styles.containerItem}>
+          <Text>Eayo</Text>
+          <Text>2285 LAKESHORE DRIVE, BUILDING 4</Text>
+          <Text>Sarette Melling</Text>
+        </View>
+        <View style={styles.containerItem}>
+          <Text>Realcube</Text>
+          <Text>Axel-Springer-Platz 3</Text>
+          <Text>Lorie Aguilar</Text>
+        </View>
+        <View style={styles.containerItem}>
+          <Text>Skyba</Text>
+          <Text>Bredgade 10</Text>
+          <Text>Hernando Downham</Text>
+        </View>
+        <View style={styles.containerItem}>
+          <Text>Tagcat</Text>
+          <Text>Carnegieplein 4</Text>
+          <Text>Anitra Halfhead</Text>
+        </View>
+        <View style={styles.containerItem}>
+          <Text>Centidel</Text>
+          <Text>150 1ST STREET S E</Text>
+          <Text>Carri Verbruggen</Text>
+        </View>
+        <View style={styles.containerItem}>
+          <Text>Eayo</Text>
+          <Text>2285 LAKESHORE DRIVE, BUILDING 4</Text>
+          <Text>Sarette Melling</Text>
+        </View>
+        <View style={styles.containerItem}>
+          <Text>Realcube</Text>
+          <Text>Axel-Springer-Platz 3</Text>
+          <Text>Lorie Aguilar</Text>
+        </View>
+        <View style={styles.containerItem}>
+          <Text>Skyba</Text>
+          <Text>Bredgade 10</Text>
+          <Text>Hernando Downham</Text>
+        </View>
+        <View style={styles.containerItem}>
+          <Text>Tagcat</Text>
+          <Text>Carnegieplein 4</Text>
+          <Text>Anitra Halfhead</Text>
+        </View>
+        <View style={styles.containerItem}>
+          <Text>Centidel</Text>
+          <Text>150 1ST STREET S E</Text>
+          <Text>Carri Verbruggen</Text>
+        </View>
+        <View style={styles.containerItem}>
+          <Text>Eayo</Text>
+          <Text>2285 LAKESHORE DRIVE, BUILDING 4</Text>
+          <Text>Sarette Melling</Text>
+        </View>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +92,19 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1
+    , flexDirection: 'column'
+    , backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'},
+  heading1 : {
+    fontSize: 30,
+    fontWeight: 'bold'},
+  containerItem: {
+    borderWidth: 1
+    , margin: 5
+    , padding: 5
+    , flex: 1
+    , width: 'auto'
+    , minWidth: 300}
 });
